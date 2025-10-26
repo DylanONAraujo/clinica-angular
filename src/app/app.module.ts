18544a9860078c666227ac233ee0d3fea7744f3f
Dyllan-a.inc <157545238+DylanONAraujo@users.noreply.github.com>
2025-10-26 18:19:53 -0300

    DevelopDylan-Adição dos botão adicionar na pagina Medico e Cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,24 @@ import { HttpClientModule } from '@angular/common/http';
 import { CustomLayoutModule } from './views/custom-layout/custom-layout.module';
 import { ClienteComponent } from './views/pages/cliente/cliente/cliente.component';
 import { MedicoComponent } from './views/pages/medico/medico/medico.component';
+import { ModalCadastrarClienteComponent } from './core/lib/components/modal-cadastrar-cliente/modal-cadastrar-cliente/modal-cadastrar-cliente.component';
+import { ModalCadastrarMedicoComponent } from './core/lib/components/modal-cadastrar-medico/modal-cadastrar-medico/modal-cadastrar-medico.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { IconModule } from '@visurel/iconify-angular';
 
 @NgModule({
-  declarations: [AppComponent, ClienteComponent, MedicoComponent],
+  declarations: [AppComponent, ClienteComponent, MedicoComponent, ModalCadastrarClienteComponent, ModalCadastrarMedicoComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    MatDialogModule,
+    MatIconModule,
+    MatButtonModule,
+    IconModule,
 
     // Vex
     VexModule,
--- src/app/views/pages/medico/medico/medico.component.ts
+++ src/app/views/pages/medico/medico/medico.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalCadastrarMedicoComponent } from '../../../../core/lib/components/modal-cadastrar-medico/modal-cadastrar-medico/modal-cadastrar-medico.component';
 
 @Component({
   selector: 'vex-medico',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
+
+  openDialog(){
+      const dialogRef = this.dialog.open(ModalCadastrarMedicoComponent, {width: '600px'})
+    }
 
 }
