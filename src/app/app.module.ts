375a1db95aed7024b58791d8c3ba9b578054110b
Dyllan-a.inc <157545238+DylanONAraujo@users.noreply.github.com>
2025-10-26 22:44:44 -0300

    developdylan-adição de modal medico e modificações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,12 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { IconModule } from '@visurel/iconify-angular';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [AppComponent, ClienteComponent, MedicoComponent, ModalCadastrarClienteComponent, ModalCadastrarMedicoComponent],
@@ -27,6 +33,13 @@ import { IconModule } from '@visurel/iconify-angular';
     MatIconModule,
     MatButtonModule,
     IconModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    MatInputModule,
 
     // Vex
     VexModule,
--- src/app/core/lib/components/modal-cadastrar-cliente/modal-cadastrar-cliente/modal-cadastrar-cliente.component.ts
+++ src/app/core/lib/components/modal-cadastrar-cliente/modal-cadastrar-cliente/modal-cadastrar-cliente.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { stagger60ms } from '../../../../../../@vex/animations/stagger.animation';
+import { fadeInUp400ms } from '../../../../../../@vex/animations/fade-in-up.animation';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'vex-modal-cadastrar-cliente',
@@ -7,7 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalCadastrarClienteComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private cd: ChangeDetectorRef,private fb: FormBuilder) {
+    this.form = this.fb.group({
+      nome: ['', Validators.required],
+      cpf: ['', Validators.required],
+      dtNascimento: ['', Validators.required],
+      sexo: [''],
+      telefone: ['', Validators.required],
+      cep: ['', Validators.required],
+      estado: ['', Validators.required],
+      cidade: ['', Validators.required],
+      bairro: ['', Validators.required],
+      complemento: ['', Validators.required],
+      logradouro: ['', Validators.required],
+      numero: ['', Validators.required],
+    });
+   }
 
   ngOnInit(): void {
   }
